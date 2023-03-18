8f3b82c61d125ab52c385d212c60f45bba6dbdb3
nishanthan <nishanthan@codevus.com>
2023-03-18 21:03:15 +0530

    read employees

--- lib/screens/employee_list.dart
+++ lib/screens/employee_list.dart
@@ -9,30 +9,30 @@ import 'package:warehouse_app/screens/update_employee.dart';
 
 class EmployeeList extends StatelessWidget {
   final CollectionReference _referance =
-      FirebaseFirestore.instance.collection('students');
+      FirebaseFirestore.instance.collection('employees');
 
   List<Employee> employees = [
-    Employee(
-        id: "2662161",
-        name: "Mathy",
-        email: "mathy@gmail.com",
-        phone: 8566,
-        position: "Intern",
-        salary: 30000),
-    Employee(
-        id: "2662161",
-        name: "Mathy",
-        email: "mathy@gmail.com",
-        phone: 8566,
-        position: "Intern",
-        salary: 70000),
-    Employee(
-        id: "2662161",
-        name: "Mathy",
-        email: "mathy@gmail.com",
-        phone: 8566,
-        position: "Intern",
-        salary: 60000)
+    // Employee(
+    //     id: "2662161",
+    //     name: "Mathy",
+    //     email: "mathy@gmail.com",
+    //     phone: 8566,
+    //     position: "Intern",
+    //     salary: 30000),
+    // Employee(
+    //     id: "2662161",
+    //     name: "Mathy",
+    //     email: "mathy@gmail.com",
+    //     phone: 8566,
+    //     position: "Intern",
+    //     salary: 70000),
+    // Employee(
+    //     id: "2662161",
+    //     name: "Mathy",
+    //     email: "mathy@gmail.com",
+    //     phone: 8566,
+    //     position: "Intern",
+    //     salary: 60000)
   ];
 
   @override
@@ -42,67 +42,37 @@ class EmployeeList extends StatelessWidget {
         title: const Text("Student Management"),
         centerTitle: true,
       ),
-      body: employees.isEmpty
-          ? const Center(
-              child: Text(
-                "No Employees \n Click + button to add new",
-                textAlign: TextAlign.center,
-              ),
-            )
-          : ListView.builder(
-              itemCount: employees.length,
-              itemBuilder: (context, index) => Card(
-                color: employees[index].salary < 35000
-                    ? Colors.red.shade200
-                    : employees[index].salary < 65000
-                        ? Colors.yellow.shade200
-                        : Colors.green.shade200,
-                child: ListTile(
-                  title: Text(employees[index].name),
-                  subtitle: Text("Mobile No : ${employees[index].phone}"),
-                  leading: CircleAvatar(
-                    radius: 25,
-                    child: Text('${employees[index].email}'),
-                  ),
-                  trailing: SizedBox(
-                    width: 60,
-                    child: Row(
-                      children: [
-                        InkWell(
-                          child: Icon(
-                            Icons.edit,
-                            color: Colors.black.withOpacity(0.75),
-                          ),
-                          onTap: () {
-                            // Navigator.push(
-                            //     context,
-                            //     MaterialPageRoute(
-                            //         builder: (context) => UpdateEmployee(
-                            //             employee: employees[index])));
-                          },
-                        ),
-                        InkWell(
-                          child: Icon(
-                            Icons.delete,
-                            color: Colors.red.withOpacity(0.75),
-                          ),
-                          onTap: () {
-                            //
-                            _referance.doc(employees[index].id).delete();
-                            //re load the page
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => EmployeeList()));
-                            //popup message
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
+      body: FutureBuilder<QuerySnapshot>(
+        future: _referance.get(),
+        builder: (context, snapshot) {
+          //check for error
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text("Somthing went wrong."),
+            );
+          }
+          //if data resived
+          if (snapshot.hasData) {
+            QuerySnapshot querySnapshot = snapshot.data!;
+            List<QueryDocumentSnapshot> documents = querySnapshot.docs;
+            //convert to list
+            List<Employee> employees = documents
+                .map((e) => Employee(
+                    id: e['id'],
+                    name: e['name'],
+                    email: e['email'],
+                    phone: e['phone'],
+                    position: e['position'],
+                    salary: e['salary']))
+                .toList();
+            return _getBody(employees);
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: (() {
           Navigator.push(context,
@@ -111,5 +81,69 @@ class EmployeeList extends StatelessWidget {
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  Widget _getBody(employees) {
+    return employees.isEmpty
+        ? const Center(
+            child: Text(
+              "No Employees \n Click + button to add new",
+              textAlign: TextAlign.center,
+            ),
+          )
+        : ListView.builder(
+            itemCount: employees.length,
+            itemBuilder: (context, index) => Card(
+              color: employees[index].salary < 35000
+                  ? Colors.red.shade200
+                  : employees[index].salary < 65000
+                      ? Colors.yellow.shade200
+                      : Colors.green.shade200,
+              child: ListTile(
+                title: Text(employees[index].name),
+                subtitle: Text("Mobile No : ${employees[index].phone}"),
+                leading: CircleAvatar(
+                  radius: 25,
+                  child: Text('${employees[index].email}'),
+                ),
+                trailing: SizedBox(
+                  width: 60,
+                  child: Row(
+                    children: [
+                      InkWell(
+                        child: Icon(
+                          Icons.edit,
+                          color: Colors.black.withOpacity(0.75),
+                        ),
+                        onTap: () {
+                          // Navigator.push(
+                          //     context,
+                          //     MaterialPageRoute(
+                          //         builder: (context) => UpdateEmployee(
+                          //             employee: employees[index])));
+                        },
+                      ),
+                      InkWell(
+                        child: Icon(
+                          Icons.delete,
+                          color: Colors.red.withOpacity(0.75),
+                        ),
+                        onTap: () {
+                          //
+                          _referance.doc(employees[index].id).delete();
+                          //re load the page
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => EmployeeList()));
+                          //popup message
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
   }
 }
